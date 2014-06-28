7d21131d36dbee85cd918944562eca42c5f244df
Jodie Belgrave <rmb1993@gmail.com>
2014-06-28 17:01:13 -0400

    added log4j

--- Network Controller/src/main/java/com/rmb938/mn2/docker/NetworkController.java
+++ Network Controller/src/main/java/com/rmb938/mn2/docker/NetworkController.java
@@ -1,9 +1,18 @@
 package com.rmb938.mn2.docker;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class NetworkController {
 
-    public static void main(String[] args) {
+    private static final Logger logger = LogManager.getLogger(NetworkController.class.getName());
 
+    public static void main(String[] args) {
+        new NetworkController();
+    }
+
+    public NetworkController() {
+        logger.info("Starting Network Controller");
     }
 
 }
