39847305f42384bfacfe9765b1a5cb00a031f1bf
Jodie <rmb1993@gmail.com>
2014-07-09 15:51:59 -0400

    log4j2

--- NetworkController/src/main/java/com/rmb938/mn2/docker/nc/NetworkController.java
+++ NetworkController/src/main/java/com/rmb938/mn2/docker/nc/NetworkController.java
@@ -3,7 +3,18 @@ package com.rmb938.mn2.docker.nc;
 public class NetworkController {
 
     public static void main(String[] args) {
+        new NetworkController();
+    }
 
+
+
+    public NetworkController() {
+        System.getenv("MONGO_URL");
+        System.getenv("RABBITMQ_URL");
+        System.getenv("REDIS_IP");
+        System.getenv("REDIS_PORT");
+        System.getenv("ETCD_IP");
+        System.getenv("ETCD_PORT");
     }
 
 }
