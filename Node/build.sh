577d9acb48ef9c5019d6f3a99a1afe9e7e3251d6
Jodie <rmb1993@gmail.com>
2014-07-22 10:34:53 -0400

    fix cmd

--- Node/build.sh
+++ Node/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Building Node Controller Image"
+docker rmi mnsquared/nodecontroller
 docker build -t="mnsquared/nodecontroller" --rm=true .
 echo "Finished building Node Controller Image"
