b2207d6c0db29f10562f4fc811bff40a6e513a93
Jodie <rmb1993@gmail.com>
2014-07-22 12:26:19 -0400

    don't force remove cache

--- Node/build.sh
+++ Node/build.sh
@@ -2,5 +2,5 @@
 
 echo "Building Node Controller Image"
 docker rmi mnsquared/nodecontroller
-docker build -t="mnsquared/nodecontroller" --rm=true .
+docker build -t="mnsquared/nodecontroller" .
 echo "Finished building Node Controller Image"
