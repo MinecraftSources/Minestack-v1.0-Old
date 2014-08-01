1920f77a81ded27493fdd8ded39a3b29fe0b05ec
Jodie Belgrave <rmb1993@gmail.com>
2014-08-01 18:50:13 -0400

    use cloudfuse mount point instead of pyrax

--- Server/run.py
+++ Server/run.py
@@ -1,7 +1,6 @@
 #!/bin/python
 import os
 import sys
-import pyrax
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
@@ -9,14 +8,6 @@ def modifyConfig(expression, value):
     print('Modifying '+expression+' with value '+str(value))
 
 def main():
-    pyrax.set_setting("identity_type", "rackspace")
-    pyrax.set_default_region('IAD')
-    pyrax.set_credentials(os.environ['RACKSPACE_USERNAME'], os.environ['RACKSPACE_API'])
-    cf = pyrax.cloudfiles
-
-    serverContainer = cf.create_container("mn2_server")
-    pluginContainer = cf.create_container("mn2_plugins")
-    worldsContainer = cf.create_container("mn2_worlds")
 
     mongoHosts = os.environ['MONGO_HOSTS'].split(',')
     mongoDB = "mn2"
@@ -62,29 +53,18 @@ def main():
         plugins.append(pluginDict)
         print('Loaded '+plugin['name']+' '+pluginConfig['name'])
 
-    print('Downloading Main Server files')
-    objects = serverContainer.get_objects()
-    for obj in objects:
-        if obj.content_type == 'application/directory':
-            continue
-        print obj.name
-        obj.download('./')
-    #os.system('ls -l server')
+    print('Copying Main Server files')
+    os.system('cp /mnt/cloudfiles/mn2_server/* .')
 
     defaultWorld = None
     os.system('mkdir worlds')
     for worldInfo in worlds:
         world = worldInfo['world']
         default = worldInfo['default']
+        print('Copying world '+world['name'])
         if default is True:
             defaultWorld = world
-            print('Downloading world '+world['name'])
-        objects = worldsContainer.get_objects(prefix=world['folder'])
-        for obj in objects:
-            if obj.content_type == 'application/directory':
-                continue
-            print obj.name
-            obj.download('worlds')
+        os.system('cp /mnt/cloudfiles/mn2_worlds/'+world['folder']+' worlds/')
     #os.system('ls -l worlds')
 
     if defaultWorld is None:
@@ -100,16 +80,11 @@ def main():
         plugin = pluginInfo['plugin']
         config = pluginInfo['config']
         print('Downloading plugin '+plugin['name'] + ' config '+config['name'])
-        objects = pluginContainer.get_objects(prefix=plugin['baseFolder'])
-        for obj in objects:
-            if obj.content_type == 'application/directory':
-                continue
-            obj.download('tempPlugins')
+
+        os.system('cp /mnt/cloudfiles/mn2_plugins/'+plugin['baseFolder']+'/* tempPlugins/'+plugin['name'])
         if config is not None:
             os.system('mv tempPlugins/'+config['location']+' tempPlugins/'+plugin['name']+'/'+plugin['configFolder'])
-        #os.system('ls -l tempPlugins/'+plugin['name'])
         os.system('mv tempPlugins/'+plugin['name']+'/* plugins')
-    #os.system('rm -rf tempPlugins')
     #os.system('ls -l plugins')
 
     #modify server config for num of players
